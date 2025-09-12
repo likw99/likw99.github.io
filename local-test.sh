19b5c35859e2720d50babd94c41428f5de040561
K <likaiwei99@gmail.com>
2025-09-12 12:40:41 +0800

    Add local test script for Jekyll site

---
+++ local-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
+# Local test script for Jekyll site
+bundle exec jekyll serve
